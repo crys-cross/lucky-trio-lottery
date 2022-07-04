855347f97f17875f01dc324992ce6c8a241b0e9d
CRYS <mariano.christopher123@gmail.com>
2022-07-04 22:45:19 +0800

    Edit test script

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -6,30 +6,68 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
-contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
-    //Raffle Variable
-   uint256 s_playersNumber; 
+/*is VRFConsumerBaseV2, KeeperCompatibleInterface*/
+contract Lottery {
+    //Lottery Variable
+    uint256 private s_playersNumber;
+    address private s_players;
 
-   //Struct
-   struct Entries {
-    uint256 s_playersNumber;
-    address s_player;
-   }
+    //Chainlink Variables
+    // uint256 private immutable i_entranceFee;
+    // address payable[] private s_players;
+    // VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    // bytes32 private immutable i_gasLane;
+    // uint64 private immutable i_subscriptionId;
+    // uint32 private immutable i_callbackGasLimit;
+    // uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    // uint32 private constant NUM_WORDS = 1;
 
-   //Array
-   Entries[] public entries;
+    //Struct
+    struct Entries {
+        uint256 s_playersNumber;
+        address s_player;
+    }
 
-   //Mapping
-   mapping(address => uint256) public s_numberOfPlayers
+    //Array
+    Entries[] public entries;
 
-    constructor() VRFConsumerBaseV2(vrfCoordinatorV2) {}
+    //Mapping
+    mapping(address => uint256) public s_playersChosenNumber;
+
+    // constructor(
+    //     address vrfCoordinatorV2,
+    //     uint256 entranceFee,
+    //     bytes32 gasLane,
+    //     uint64 subscriptionId,
+    //     uint32 callbackGasLimit,
+    //     uint256 interval
+    // ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    //     i_entranceFee = entranceFee;
+    //     i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+    //     i_gasLane = gasLane;
+    //     i_subscriptionId = subscriptionId;
+    //     i_callbackGasLimit = callbackGasLimit;
+    // }
+    function getEntry(uint256 playersNumber, address player) internal {
+        s_playersChosenNumber[player] = playersNumber;
+    }
 
     function storeNumber(uint256 playersNumber) public {
-        s_playersNumber = playersNumber
+        s_playersNumber = playersNumber;
     }
 
-    function addPerson(string memory player, uint256 playersNumber) public {
-        entries.push(People(playersNumber, player));
-        s_numberOfPlayers[player] = playersNumber;
+    function storePlayer(address players) public {
+        s_players = address(payable(msg.sender));
     }
+
+    //     function addEntry(address player, uint256 playersNumber) public {
+    //         entries.push(Entries(playersNumber, msg.sender));
+    //         s_playersChosenNumber[player] = playersNumber;
+    //     }
 }
+
+//todo
+//map player address to number
+//function to choose lucky number
+//array to get addresses who chose the lucky number
+//function to divide potmoney and send to all winners
